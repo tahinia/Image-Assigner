e1f07950cadb9d976af0514a837da90e734f98e1
Taghinia <keivan.taghinia@gmail.com>
2021-08-16 16:37:14 +0100

    new styles added and image now shows up

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const imagebox = document.getElementById("image-box");
 const refresh = document.querySelector('.refresh');
+const newImage = document.createElement('img');
+const topLoader = document.getElementById('top-loader-container');
 
+
+function createImage(){
 axios.get("https://picsum.photos/200/300")
 .then(function(response) {
+    const picID = response.headers['picsum-id'];
+    let picUrl = `https://picsum.photos/id/${picID}/500`;
+    imagebox.insertBefore(newImage, refresh);
+    newImage.setAttribute(`class`, `randomImage`);
+    newImage.setAttribute(`src`, picUrl);
+    newImage.setAttribute(`alt`, `Picsum Image`);
+    topLoader.classList.add('loading');
     console.log(response);
 })
+.catch(function(error) {
+    console.log(error);
+})
+.finally(function(response){
+    topLoader.classList.remove('loading');
+});
+}
+
+createImage();
+
+
+refresh.addEventListener('click', function() {
+    topLoader.classList.add('loading');
+    createImage();
+});
